59a2937f9a8a0073d4339f864db47c92dcfaf74c
sahion <asuvorov-ki19@stud.sfu-kras.ru>
2020-04-30 22:57:07 +0700

    Add clear button

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const container = document.querySelector('#container');
-let amount=16;
-let gridTemplateColumns='';
+
+
+function createBoard(){
 for (i=0;i<amount;i++){
 gridTemplateColumns+='auto ';
 }
@@ -13,4 +13,30 @@ for (i=0;i<amount*amount;i++){
  
  container.appendChild(block);
 }
+}
 
+function changeColor(e){
+    this.style.cssText='background-color:black;';
+}
+
+
+
+function clear(e){
+    boxes.forEach(box => box.style.background="aqua");
+}
+
+
+const container = document.querySelector('#container');
+let amount=16;
+let gridTemplateColumns='';
+
+
+
+
+
+createBoard();
+const boxes = document.querySelectorAll('.box');
+const btnClear = document.querySelector('#clear');
+
+btnClear.addEventListener('click',clear);
+boxes.forEach(box => box.addEventListener('mouseover', changeColor));
