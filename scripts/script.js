4f4031fe01180930ffb7fe4df15765d056fd79c1
sahion <asuvorov-ki19@stud.sfu-kras.ru>
2020-04-30 22:15:37 +0700

    make grid great again

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,16 @@
 const container = document.querySelector('#container');
 let amount=16;
-for (i=0;i<amount*amount;i++){
+let gridTemplateColumns='';
+for (i=0;i<amount;i++){
+gridTemplateColumns+='auto ';
+}
+container.style.cssText=`grid-template-columns:${gridTemplateColumns}; `;
 
+for (i=0;i<amount*amount;i++){
  const block =  document.createElement('div');
+ 
  block.classList.add('box');
+ 
  container.appendChild(block);
 }
+
